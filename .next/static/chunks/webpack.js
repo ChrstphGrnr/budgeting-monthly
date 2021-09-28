4320a615009a171e3321b06dfa3986465fe8a2b3
ChrstphGrnr <cgruener@quantummetric.com>
2021-09-28 09:21:01 -0400

    dynamic post routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "166cf4452dc699a48255"; }
+/******/ 		__webpack_require__.h = function() { return "75ca1dc0acff0c4319aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
